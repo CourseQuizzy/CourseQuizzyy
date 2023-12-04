539352fff5dfc44a94a9e2c71f35759e788336df
figoyudhistiraw <figoy2@gmail.com>
2023-12-04 08:11:01 +0700

    add login and register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Home from "./pages/HomePage/Home";
 import Catalogue from "./pages/CourseCatalogue/CourseCatalogue";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CourseHomePage from "./pages/CourseHomePage/CourseHomePage";
+import LoginPage from "./pages/AuthPage/LoginPage"
+import SignUpPage from "./pages/AuthPage/SignUpPage";
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +12,9 @@ function App() {
         <Route path="/" exact element={<Home />} />
         <Route path="/catalogue" exact element={<Catalogue />} />
         <Route path="/courses/1" exact element={<CourseHomePage />} />
+        <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/register" exact element={<SignUpPage />} />
+
       </Routes>
     </BrowserRouter>
   );
