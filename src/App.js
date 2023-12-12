cfba537f9cb946a838a463fd9f06721a43e3cc43
figoyudhistiraw <figoy2@gmail.com>
2023-12-12 11:36:12 +0700

    update auth page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CourseHomePage from "./pages/CourseHomePage/CourseHomePage";
 import LoginPage from "./pages/AuthPage/LoginPage"
 import SignUpPage from "./pages/AuthPage/SignUpPage";
+import AddCourseStep1 from "./pages/CreateCourse/AddCourse";
+import AddCourseStep2 from "./pages/CreateCourse/AddCourse2";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/courses/1" exact element={<CourseHomePage />} />
         <Route path="/login" exact element={<LoginPage />} />
         <Route path="/register" exact element={<SignUpPage />} />
+        <Route path="/courses/create" exact element={<AddCourseStep1 />} />
+        <Route path="/courses/create2" exact element={<AddCourseStep2 />} />
 
       </Routes>
     </BrowserRouter>
